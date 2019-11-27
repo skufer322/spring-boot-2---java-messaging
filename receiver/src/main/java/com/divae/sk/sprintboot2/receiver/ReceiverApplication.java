6eb34e8e36a1e7da62fbf07bc365d50a7f11fe5d
Stefan Kufer <stefan.kufer@diva-e.com>
2019-11-27 10:51:04 +0100

    added receiver module

---
+++ receiver/src/main/java/com/divae/sk/sprintboot2/receiver/ReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.divae.sk.sprintboot2.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiverApplication.class, args);
+    }
+
+}
