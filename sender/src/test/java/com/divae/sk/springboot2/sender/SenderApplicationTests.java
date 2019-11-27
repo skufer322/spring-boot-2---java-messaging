72aa44809beb7fa5f8e9f6e03e8637254773684f
Stefan Kufer <stefan.kufer@diva-e.com>
2019-11-27 10:52:32 +0100

    added sender module

---
+++ sender/src/test/java/com/divae/sk/springboot2/sender/SenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divae.sk.springboot2.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
